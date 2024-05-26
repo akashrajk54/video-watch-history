c766b260b9db0d4713792ef361456032b60a431c
akashrajk545 <akashrajk54@gmail.com>
2024-05-26 18:18:19 +0530

    Update > authentication completed using JWT token.

---
+++ accounts_engine/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from accounts_engine.models import (CustomUser, InvalidatedToken)
+
+# Register your models here.
+admin.site.register(CustomUser)
+admin.site.register(InvalidatedToken)
+
